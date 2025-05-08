e826f6f66d7e1e2f82be48fd27b4ba31f8d6bf3b
bossyobro <bossyobro123@gmail.com>
2025-05-08 14:09:02 +0200

    Actually applying it to main.py oops it was in test.py

--- main.py
+++ main.py
@@ -1,139 +1,170 @@
 from db import *
 from os import system
 
-def print_ui(user, logged_in):
 
-    if logged_in == False:
-        UserAuth()
-    else:
-        mycursor.execute("SELECT user_type FROM brukere WHERE username = %s", (user[1],))
+class InventoryApp:
+    def __init__(self):
+        self.user = None
+        self.logged_in = False
+
+    def run(self):
+        self.user_auth()
+
+    def user_auth(self):
+        print("Velkommen til Varehåndteringssystemet!")
+        print("Vennligst logg inn for å fortsette.")
+        username = input("Brukernavn: ")
+        password = input("Passord: ")
+
+        mycursor.execute("SELECT * FROM brukere WHERE username = %s AND password = %s", (username, password))
+        user = mycursor.fetchone()
+
+        if user:
+            self.user = user
+            self.logged_in = True
+            print("Login Sucessfull!")
+            self.print_ui()
+        else:
+            print("Login Unsucessfull, try again!")
+            input("Press enter to continue... ")
+            self.user_auth()
+
+    def print_ui(self):
+        mycursor.execute("SELECT user_type FROM brukere WHERE username = %s", (self.user[1],))
         user_type = mycursor.fetchone()
 
-    print(user_type[0])
-
-    if user_type[0] == "Admin":
-        admin_ui()
-    else:
-        worker_ui()
-
-
-
-
-
-
-def worker_ui():
-     while True:
-        # system("cls")
-        print("Velkommen til Varehåndteringssystemet!")
-        print("1. Vis alle varer")
-        print("2. Søk")
-        print("3. Oppdatere en vare")
-        print("4. Avslutt")
-        userInput = int(input("Pick from 1-4: "))
-
-
-        if userInput == 1:
-            view_items()
-        elif userInput == 2:
-            search_items()
-        elif userInput == 3:
-            update_storage()
-        elif userInput == 4:
-            quit_program()
+        if user_type[0] == "Admin":
+            self.admin_ui()
         else:
-            print("Ugyldig valg, prøv igjen.")
+            self.worker_ui()
 
+    def worker_ui(self):
+        while True:
+            print("Velkommen til Varehåndteringssystemet!")
+            print("1. Vis alle varer")
+            print("2. Søk")
+            print("3. Oppdatere en vare")
+            print("4. Avslutt")
+            userInput = int(input("Pick from 1-4: "))
 
+            if userInput == 1:
+                self.view_items()
+            elif userInput == 2:
+                self.search_items()
+            elif userInput == 3:
+                self.update_storage()
+            elif userInput == 4:
+                self.quit_program()
+            else:
+                print("Ugyldig valg, prøv igjen.")
 
-def admin_ui():
-    while True:
-        # system("cls")
-        print("Velkommen til Varehåndteringssystemet!")
-        print("1. Legg til en ny vare")
-        print("2. Vis alle varer")
-        print("3. Søk")
-        print("4. Oppdatere en vare")
-        print("5. Slett en vare")
-        print("6. Avslutt")
-        userInput = int(input("Pick from 1-6: "))
+    def admin_ui(self):
+        while True:
+            print("Velkommen til Varehåndteringssystemet!")
+            print("1. Legg til en ny vare")
+            print("2. Vis alle varer")
+            print("3. Søk")
+            print("4. Oppdatere en vare")
+            print("5. Slett en vare")
+            print("6. Avslutt")
+            userInput = int(input("Pick from 1-6: "))
 
-        if userInput == 1:
-            add_item()
-        elif userInput == 2:
-            view_items()
-        elif userInput == 3:
-            search_items()
-        elif userInput == 4:
-            update_storage()
-        elif userInput == 5:
-            delete_items()
-        elif userInput == 6:
-            quit_program()
+            if userInput == 1:
+                self.add_item()
+            elif userInput == 2:
+                self.view_items()
+            elif userInput == 3:
+                self.search_items()
+            elif userInput == 4:
+                self.update_storage()
+            elif userInput == 5:
+                self.delete_items()
+            elif userInput == 6:
+                self.quit_program()
+            else:
+                print("Ugyldig valg, prøv igjen.")
+
+    def view_items(self):
+        mycursor.execute("SELECT * FROM varer")
+        varer = mycursor.fetchall()
+        if varer:
+            print("Varer i lageret:")
+            print(f"{'Navn':<20}{'Varenummer':<15}{'Pris':<10}{'Antall':<10}{'Kategori':<15}")
+            print("-" * 70)
+            for vare in varer:
+                print(f"{vare[0]:<20}{vare[1]:<15}{vare[2]:<10}{vare[3]:<10}{vare[4]:<15}")
         else:
-            print("Ugyldig valg, prøv igjen.")
+            print("Ingen varer funnet i lageret.")
+        self.return_to_menu()
 
-def delete_items():
-    print("Hvilken vare har du lyst til å slette?")
-    varenummer = input("Varenummer: ")
+    def search_items(self):
+        varenummer = input("Skriv inn varenummer for å søke: ")
+        mycursor.execute("SELECT * FROM varer WHERE varenummer = %s", (varenummer,))
+        result = mycursor.fetchone()
+        if result:
+            print("Vare funnet:")
+            print(f"Navn: {result[0]}, Varenummer: {result[1]}, Pris: {result[2]}, Antall: {result[3]}, Kategori: {result[4]}")
+        else:
+            print("Ingen vare funnet med det oppgitte varenummeret.")
+        self.return_to_menu()
 
-    mycursor.execute("SELECT * FROM varer WHERE varenummer = %s", (varenummer,))
-    item = mycursor.fetchone()
-
-    if item:
-        print(f"Vare funnet: Navn: {item[0]}, Pris: {item[2]}, Antall: {item[3]}, Kategori: {item[4]}")
-        mycursor.execute("DELETE FROM varer WHERE varenummer = %s", (varenummer,))
-        dbconn.commit()
+    def update_storage(self):
+        varenummer = input("Hvilken vare har du lyst til å oppdatere?\nVarenummer: ")
 
         mycursor.execute("SELECT * FROM varer WHERE varenummer = %s", (varenummer,))
-        deleted_item = mycursor.fetchone()
-        if not deleted_item:
-            print("Varen ble slettet fra databasen.")
+        item = mycursor.fetchone()
+
+        if item:
+            print(f"Vare funnet: Navn: {item[0]}, Pris: {item[2]}, Antall: {item[3]}, Kategori: {item[4]}")
+            ny_pris = input("Ny pris (trykk enter for å beholde nåværende): ")
+            ny_antall = input("Nytt antall (trykk enter for å beholde nåværende): ")
+
+            if ny_pris:
+                mycursor.execute("UPDATE varer SET pris = %s WHERE varenummer = %s", (ny_pris, varenummer))
+            if ny_antall:
+                mycursor.execute("UPDATE varer SET antall = %s WHERE varenummer = %s", (ny_antall, varenummer))
+
+            dbconn.commit()
+            print("Varen ble oppdatert.")
         else:
-            print("Noe gikk galt. Varen ble ikke slettet.")
-        
-    else:
-        print("Ingen vare funnet med det oppgitte varenummeret.")
-    return_to_menu()
+            print("Ingen vare funnet med det oppgitte varenummeret.")
+        self.return_to_menu()
 
-def update_storage():
-    print("Hvilken vare har du lyst til å oppdatere?")
-    varenummer = input("Varenummer: ")
+    def delete_items(self):
+        varenummer = input("Hvilken vare har du lyst til å slette?\nVarenummer: ")
 
-    mycursor.execute("SELECT * FROM varer WHERE varenummer = %s", (varenummer,))
-    item = mycursor.fetchone()
+        mycursor.execute("SELECT * FROM varer WHERE varenummer = %s", (varenummer,))
+        item = mycursor.fetchone()
 
-    if item:
-        print(f"Vare funnet: Navn: {item[0]}, Pris: {item[2]}, Antall: {item[3]}, Kategori: {item[4]}")
-        ny_pris = input("Ny pris (trykk enter for å beholde nåværende): ")
-        ny_antall = input("Nytt antall (trykk enter for å beholde nåværende): ")
+        if item:
+            print(f"Vare funnet: Navn: {item[0]}, Pris: {item[2]}, Antall: {item[3]}, Kategori: {item[4]}")
+            mycursor.execute("DELETE FROM varer WHERE varenummer = %s", (varenummer,))
+            dbconn.commit()
 
-        if ny_pris:
-            mycursor.execute("UPDATE varer SET pris = %s WHERE varenummer = %s", (ny_pris, varenummer))
+            mycursor.execute("SELECT * FROM varer WHERE varenummer = %s", (varenummer,))
+            deleted_item = mycursor.fetchone()
+            if not deleted_item:
+                print("Varen ble slettet fra databasen.")
+            else:
+                print("Noe gikk galt. Varen ble ikke slettet.")
+        else:
+            print("Ingen vare funnet med det oppgitte varenummeret.")
+        self.return_to_menu()
 
-        if ny_antall:
-            mycursor.execute("UPDATE varer SET antall = %s WHERE varenummer = %s", (ny_antall, varenummer))
+    def add_item(self):
+        navn = input("Navn: ")
 
-        dbconn.commit()
-        print("Varen ble oppdatert.")
-    else:
-        print("Ingen vare funnet med det oppgitte varenummeret.")
+        mycursor.execute("SELECT * FROM varer WHERE navn = %s", (navn,))
+        existing_item = mycursor.fetchone()
 
-    return_to_menu()
+        if existing_item:
+            print("En vare med dette navnet eksisterer allerede. Vennligst velg et annet navn.")
+            return self.print_ui()
 
-def add_item():
-    navn = input("navn: ")
-
-    # Check if the item name already exists in the database
-    mycursor.execute("SELECT * FROM varer WHERE navn = %s", (navn,))
-    existing_item = mycursor.fetchone()
-
-    if existing_item:
-        print("En vare med dette navnet eksisterer allerede. Vennligst velg et annet navn.")
-        return print_ui()
-    else:
         pris = input("Pris: ")
         antall = input("Antall: ")
         kategori = input("A) elektronikk, B) klær, C) kontor [A/B/C]? : ")
+
         if kategori == "A":
             kategori = "elektronikk"
         elif kategori == "B":
@@ -141,95 +172,42 @@ def add_item():
         elif kategori == "C":
             kategori = "kontor"
         else:
-            print("You didn't pick any of the options! Go back as punishment!")
-            return print_ui()
+            print("Du valgte ikke et gyldig alternativ!")
+            return self.print_ui()
 
-    # Varenummer blir basically autogeneret her. Starter på 1000 og incrementer med 1 for alle nye items
-    mycursor.execute("SELECT MAX(varenummer) FROM varer")
-    result = mycursor.fetchone()
+        mycursor.execute("SELECT MAX(varenummer) FROM varer")
+        result = mycursor.fetchone()
+        varenummer = 1000 if result[0] is None else result[0] + 1
 
-    if result[0] is None:
-        varenummer = 1000
-    else:
-        varenummer = result[0] + 1
+        sql = "INSERT INTO varer (navn, varenummer, pris, antall, kategori) VALUES (%s, %s, %s, %s, %s)"
+        vals = (navn, varenummer, pris, antall, kategori)
+        mycursor.execute(sql, vals)
+        dbconn.commit()
 
-    sql_statement = "INSERT INTO varer (navn, varenummer, pris, antall, kategori) VALUES (%s, %s, %s, %s, %s)"
-    vals = (navn, varenummer, pris, antall, kategori)
-    mycursor.execute(sql_statement, vals)
-    dbconn.commit()
+        mycursor.execute("SELECT * FROM varer WHERE varenummer = %s", (varenummer,))
+        added_item = mycursor.fetchone()
 
-    # En sjekk for om den ble adda i databasen eller ikke.
-    mycursor.execute("SELECT * FROM varer WHERE varenummer = %s", (varenummer,))
-    added_item = mycursor.fetchone()
-    
-    if added_item:
-        print("Varen ble lagt til i databasen:")
-        print(added_item)
-    else:
-        print("Noe gikk galt. Varen ble ikke lagt til.")
-    
-    return_to_menu()
+        if added_item:
+            print("Varen ble lagt til i databasen:")
+            print(added_item)
+        else:
+            print("Noe gikk galt. Varen ble ikke lagt til.")
+        self.return_to_menu()
 
-def view_items():
-    mycursor.execute("SELECT * FROM varer")
-    varer = mycursor.fetchall()
-    if varer:
-        print("Varer i lageret:")
-        print(f"{'Navn':<20}{'Varenummer':<15}{'Pris':<10}{'Antall':<10}{'Kategori':<15}")
-        print("-" * 70)
-        for vare in varer:
-            print(f"{vare[0]:<20}{vare[1]:<15}{vare[2]:<10}{vare[3]:<10}{vare[4]:<15}")
-    else:
-        print("Ingen varer funnet i lageret.")
-
-    return_to_menu()
-
-def search_items():
-    varenummer = input("Skriv inn varenummer for å søke: ")
-    mycursor.execute("SELECT * FROM varer WHERE varenummer = %s", (varenummer,))
-    result = mycursor.fetchone()
-    if result:
-        print("Vare funnet:")
-        print(f"Navn: {result[0]}, Varenummer: {result[1]}, Pris: {result[2]}, Antall: {result[3]}, Kategori: {result[4]}")
-    else:
-        print("Ingen vare funnet med det oppgitte varenummeret.")
-    return_to_menu()
-
-def quit_program():
-    print("Avslutter programmet.")
-    quit()
-
-def return_to_menu():
-    enter = input("Press enter to continue... ")
-
-    if enter == "":
-        return print_ui()
-    else:
-        print("Sorry, it seems something went wrong")
+    def quit_program(self):
+        print("Avslutter programmet.")
         quit()
 
-def UserAuth():
-    # system("cls")
-    print("Velkommen til Varehåndteringssystemet!")
-    print("Vennligst logg inn for å fortsette.")
-    username = input("Brukernavn: ")
-    password = input("Passord: ")
-
-    # Sjekk brukernavn og passord i databasen
-    mycursor.execute("SELECT * FROM brukere WHERE username = %s AND password = %s", (username, password))
-    user = mycursor.fetchone()
-
-    if user:
-        logged_in = True
-        print("Login Sucessfull!")
-        print_ui(user, logged_in)
-    else:
-        print("Login Unsucessfull try again!")
-        enter = input("Press enter to continue... ")
+    def return_to_menu(self):
+        enter = input("Trykk enter for å fortsette... ")
         if enter == "":
-            return UserAuth()
-    
+            self.print_ui()
+        else:
+            print("Ugyldig inndata. Avslutter.")
+            quit()
 
-    
-UserAuth()
 
+# === Run app ===
+if __name__ == "__main__":
+    app = InventoryApp()
+    app.run()
